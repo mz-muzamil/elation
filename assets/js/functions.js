86f9e874dd6423f19f2c5a3b8ea18b988f825c16
Muhammad Muzamil <muhammad.muzamil89@gmail.com>
2023-02-18 23:28:00 +0500

    initial commit

---
+++ assets/js/functions.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  $("#main_banner").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    items: 1,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+  });
+
+  $("#testimonials").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+  });
+
+  $("#our_clients").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    items: 4,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    navText: [
+      "<i class='fa-solid fa-angle-left'></i>",
+      "<i class='fa-solid fa-angle-right'></i>",
+    ],
+  });
+
+  $(window).scroll(function () {
+    var sticky = $(".header");
+    var scroll = $(window).scrollTop();
+    if (scroll >= 400) {
+      sticky.addClass("animate__animated animate__slideInDown");
+    } else {
+      sticky.removeClass("animate__animated animate__slideInDown");
+    }
+  });
+});
